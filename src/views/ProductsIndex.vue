b44ae7d1bfed0d3ba4e2b8c444580560c512c632
Chris Taylor <ctaylor534@gmail.com>
2022-08-02 11:54:27 -0700

    Init

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/products").then((response) => {
+        this.products = response.data;
+        console.log("All Products:", this.products);
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-for="product in products" :key="product.id">
+    <h1>{{ product.name }}</h1>
+    <h4>{{ product.price }}</h4>
+  </div>
+</template>
